2c7ed9a264d58f4782db68d55557bf86a9f79743
Obed Guzman Flore <230142@utxicotepec.edu.mx>
2025-01-17 15:17:40 -0600

    Practica04
    Agregacion de codigo al archivo app.js

--- app.js
+++ app.js
@@ -1,132 +1,163 @@
 import express from 'express';
 import session from 'express-session';
+import bodyParser from 'body-parser';
 import moment  from 'moment-timezone';
+import { v4 as uuidv4 } from 'uuid';
 
-const app= express(); //HABILITA EL USO DE LOS VERBOS HTTP, GET, PST, PATHC,DELETE, ECT
 
-//La configuracion de las sesiones
-app.use(
-    session({
-        secret:'p3-Obed#OBGF-sessiionespersistentes',
+ const app= express();
+ const PORT = 3500;
+ app.use(express.json())
+
+ app.use(express.urlencoded({extended:true}))
+ 
+ //configuraxion de la sesiones
+
+ app.use(
+        session({
+        secret:'p4-Obed#OBGF-sessiionesHTTP',
         resave: false, //permite desabilitar los cambios 
-        saveUninitialized: true,  //si no esta inicializada que se inicialize
-        cookie: {maxAge: 24*60*60*100}
-    })
-)
+        saveUninitialized: false,  //si no esta inicializada que se inicialize
+        cookie: {maxAge: 5*60*100}
 
 
-//Ruta para inicializar la sesion
-app.get('/iniciar-sesion', (req,res)=>{
-    if(!req.session.inicio){
-        req.session.inicio= new Date(); //Fecha de inicio de sesion
-        req.session.ultimoacceso= new Date(); //Ultima consulta inicial
-        res.send('Sesion iniciada')
+        })
+ )
+
+// Funcion de utilidad que nos permitira acceder a la informacion de la intefaz de red en este caso (LAN)
+
+const getClientIp = (req) => {
+    return (
+        req.headers['x-forwarded-for'] || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress || 
+        req.connection.socket?.remoteAddress
         
-    }else{
-        res.send('La sesion ya esta activa')
+    );
+    
+};
+
+// Login endpoint
+
+app.post("/login", (req,res)=> {
+    console.log(req);
+    const {email, nickname , macAddress} = req.body;
+
+    if(!email || !nickname || !macAddress){
+        return res.status(400).json({ message: "Se esperan campos requeridos"});
     }
+
+
+    const sessionID= uuidv4();
+    const now = new Date();
+    
+    session[sessionID]={
+        sessionID,
+        email,
+        nickname,
+        macAddress,
+        ip:getClientIp(req),
+        createAt: now,
+        lastAccessed: now,
+
+    };
+
+    res.status(200).json({
+    message:"Se ha logeado de manera exitosa",
+    sessionID,
+
+});
+
 });
 
 
-// Ruta para actulizar la fecha de última consulta
-app.get('/actualizar', (req,res )=>{
-    if(req.session.inicio){
-        req.session.ultimoAcceso = new Date();
-        res.send('Fecha de última consulta actualizada');
-    }else{
-        res.send('No hay una sesion activa');
+// Logout endpoint
 
+app.get("/logout", (req,res)=>{
+    const {sessionID }= req.body;
+
+if(!sessionID || !session[sessionID]){
+    return res.status(404).json({
+        message: "No se ha encontrado una sesion activa."
+
+    });
+}
+
+
+
+
+delete session[sessionID];
+req.session.destroy((err) =>{
+    if(err) {
+        return res.status(500).send("Error al cerra la sesion");
     }
+
 
 })
 
+res.status(200).json({message: "Logout successful"});
 
- // Duración máxima de inactividad (en milisegundos)
- const TIEMPO_INACTIVIDAD_MAX = 5 * 60 * 1000; // 5 minutos
- 
-
- // Middleware para controlar inactividad
- app.use((req, res, next) => {
-   const ahora = Date.now();
- 
-   // Si no hay registro de actividad previa, inicializarlo
-   if (!req.session.ultimaActividad) {
-     req.session.ultimaActividad = ahora;
-     return next();
-   }
- 
-   // Calcular tiempo de inactividad
-   const tiempoInactivo = ahora - req.session.ultimaActividad;
- 
-   // Si excede el tiempo máximo permitido, destruir la sesión
-   if (tiempoInactivo > TIEMPO_INACTIVIDAD_MAX) {
-     req.session.destroy(err => {
-       if (err) {
-         return res.status(500).send('Error al cerrar sesión por inactividad.');
-       }
-       return res.status(401).send('Sesión cerrada por inactividad.');
-     });
-   } else {
-     // Si no excede, actualizar el tiempo de última actividad
-     req.session.ultimaActividad = ahora;
-     next();
-   }
- });
- 
-
-//Ruta para ver el estado de la sesion 
-app.get('/estado-sesion', (req,res)=>{
-
-    if(req.session.inicio){
-        const inicio = new Date(req.session.inicio);
-        const ultimoAcceso = new Date(req.session.ultimoAcceso);
-        const ahora = new Date();
-
-        // Calcular la antiguedad de la SESION
-        const antiguedadMS= ahora - inicio;
-        const horas= Math.floor(antiguedadMS/(1000*60*60));
-        const minutos=Math.floor(antiguedadMS%(1000*60*60)/(1000*60));
-        const segundos =Math.floor(antiguedadMS%(1000*60)/1000);
-     
-        //Convertimos las fechas al huso horario CDMX
-        const inicioCDMX= moment(inicio).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
-        const ultimoCDMX= moment(ultimoAcceso).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
-        
-
-        res.json({
-            mensaje: 'Estado de la sesion',
-            sesionID: req.sessionID,
-            Fehca_inicio_Sesion: inicioCDMX,
-            ultimoAcceso: ultimoCDMX,
-            Duracion_dela_sesion_: `${horas} horas, ${minutos} minutos, ${segundos} segundos`
-        });
-
-    } else {
-        res.send ('No hay una sesion activa.')
-    }
 });
 
-// Ruta para cerrar la sesion
- app.get('/cerrar-sesion', (req,res)=>{
-    if(req.session){
-        req.session.destroy((err)=>{
-            if(err){
-                return res.status(500).send('Error al cerrar ka sesion.');
-            }
-            res.send('Sesion cerrada correctamente.');
-        });
-    }else {
-        res.send('No hay una sesion activa para cerrar.');
+
+// Actualizacion de la Sesion
+app.put("/update", (req,res)=>{
+    const {sessionID, email, nickname }= req.body;
+    if(!sessionID || !session[sessionID]){
+        return res.status(404).json({message:" No existe una sesion activa"});
     }
- }); 
 
- //inicializar el servidor
+    if(email) session[sessionID].email =email;
+    if(nickname) session[sessionID].nickname = nickname;
+    session[sessionID].lastAccess= new Date();
 
- const PORT = 3000;
+    res.status(200).json({
+        message:"La sesion se ha actualizado",
+        sesion: session[sessionID]
+    })
 
- app.listen(PORT, ()=>{
+});
+
+
+//Estatis de la Sesion
+app.get("/status",(req,res)=>{
+    const sessionID= req.query.sessionID;
+    if(!sessionID || !session[sessionID]){
+    return res.status(404).json({message:"No hay sesion activa."});
+
+    }
+
+    res.status(200).json({
+        message:"Sesion Activa",
+        session: session[sessionID]
+
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
     console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
 
-
-
  })
+
+
+
+
+
+
