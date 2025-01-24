e240a9f0c22bc22acdc27bb7cc6b7b59a5ba5370
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2025-01-23 23:15:43 -0600

    Practica 04
    Correcion de errores

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ import session from 'express-session';
 import bodyParser from 'body-parser';
 import moment  from 'moment-timezone';
 import { v4 as uuidv4 } from 'uuid';
-import cors from 'cors'
+import cors from 'cors';
+import os from 'os';
+
 
 
  const app= express();
  const PORT = 3500;
 
- const activeSessions = {}; //sirve para que se almacene las sesiones
 
  app.use(cors({
     origin: 'http://localhost:3000', // Cambia esto al dominio permitido
@@ -23,6 +24,10 @@ import cors from 'cors'
 
  app.use(express.urlencoded({extended:true}))
  
+ const sessions = {}; //sirve para que se almacene las sesiones
+
+
+
  //configuraxion de la sesiones
 
  app.use(
@@ -39,20 +44,20 @@ import cors from 'cors'
 // Funcion de utilidad que nos permitira acceder a la informacion de la intefaz de red en este caso (LAN)
 
 const getClientIp = (req) => {
-    return (
+    const ip=
         req.headers['x-forwarded-for'] || 
         req.connection.remoteAddress || 
         req.socket.remoteAddress || 
         req.connection.socket?.remoteAddress
         
-    );
+    
     
 };
 
 // Login endpoint
 
 app.post("/login", (req,res)=> {
-    console.log(req);
+    
     const {email, nickname , macAddress} = req.body;
 
     if(!email || !nickname || !macAddress){
@@ -63,19 +68,19 @@ app.post("/login", (req,res)=> {
     const sessionID= uuidv4();
     const now = new Date();
     
-    const sessionData={
+    sessions[sessionID]={
         sessionID,
         email,
         nickname,
         macAddress,
-        ip:getClientIp(req),
+        ip:getServerNetworkInfo(),
         createAt: now,
         lastAccessed: now,
 
     };
 
-    req.session[sessionID] = sessionData;
-    activeSessions[sessionID] = sessionData; //Guardar sesion en el almacenamiento de sesiones
+    // req.session[sessionID] = sessionData;
+    // activeSessions[sessionID] = sessionData; //Guardar sesion en el almacenamiento de sesiones
 
     res.status(200).json({
     message:"Se ha logeado de manera exitosa",
@@ -88,10 +93,10 @@ app.post("/login", (req,res)=> {
 
 // Logout endpoint
 
-app.get("/logout", (req,res)=>{
+app.post("/logout", (req,res)=>{
     const {sessionID }= req.body;
 
-if(!sessionID || !session[sessionID]){
+if(!sessionID || !sessions[sessionID]){
     return res.status(404).json({
         message: "No se ha encontrado una sesion activa."
 
@@ -101,7 +106,7 @@ if(!sessionID || !session[sessionID]){
 
 
 
-delete activeSessions[sessionID];
+delete sessions[sessionID];
 req.session.destroy((err) =>{
     if(err) {
         return res.status(500).send("Error al cerra la sesion");
@@ -116,20 +121,24 @@ res.status(200).json({message: "Logout successful"});
 
 
 // Actualizacion de la Sesion
-app.put("/update", (req,res)=>{
+app.post("/update", (req,res)=>{
+    
+
     const {sessionID, email, nickname }= req.body;
-    if(!sessionID || !activeSessions[sessionID]){
+    if(!sessionID || !sessions[sessionID]){
         return res.status(404).json({message:" No existe una sesion activa"});
     }
 
-    if(email) activeSessions[sessionID].email =email;
-    if(nickname) activeSessions[sessionID].nickname = nickname;
-    activeSessions[sessionID].lastAccess= new Date();
+    if(email) sessions[sessionID].email =email;
+    if(nickname) sessions[sessionID].nickname = nickname;
+    sessions[sessionID].lastAccesed= new Date();
 
     res.status(200).json({
         message:"La sesion se ha actualizado",
-        sesion: activeSessions[sessionID]
+        sesion: sessions[sessionID]
     })
+    console.log("Sesiones activas:", sessions);
+console.log("SessionID proporcionado:", sessionID);
 
 });
 
@@ -137,14 +146,14 @@ app.put("/update", (req,res)=>{
 //Estatis de la Sesion
 app.get("/status",(req,res)=>{
     const sessionID= req.query.sessionID;
-    if(!sessionID || !activeSessions[sessionID]){
+    if(!sessionID || !sessions[sessionID]){
     return res.status(404).json({message:"No hay sesion activa."});
 
     }
 
     res.status(200).json({
         message:"Sesion Activa",
-        session: activeSessions[sessionID]
+        session: sessions[sessionID]
 
 
     })
@@ -155,21 +164,23 @@ app.get("/status",(req,res)=>{
 app.get('/sessions', (req, res) => {
     res.status(200).json({
         message: 'Sesiones activas',
-        sessions: Object.values(activeSessions), // Retornar todas las sesiones activas
+        sessions: Object.values(sessions), // Retornar todas las sesiones activas
     });
 });
 
 
 
 
-
-
-
-
-
-
-
-
+const getServerNetworkInfo = () => {
+    const interfaces = os.networkInterfaces()
+    for(const name in interfaces){
+        for(const iface of interfaces[name]){
+            if(iface.family === 'IPv4' && !iface.internal ){
+                return{serverIP: iface.address, serverMac: iface.mac}
+            }
+        }
+    }
+}
 
 app.listen(PORT, ()=>{
     console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
